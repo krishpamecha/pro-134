5ef5b026b1df9505d21e8ade3ff4ecd88fd255a1
Krish <89772346+krishpamecha@users.noreply.github.com>
2023-08-31 16:18:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+img = "";
+status = "";
+objects = [];
+
+
+
+function setup() {
+    canvas=createCanvas(700, 500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(700, 500);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 700, 500);
+    if (status != "person") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Person Found" + objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+    else {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number_of_objects").innerHTML = "Person Not Found" + objects.length;
+    }
+}
